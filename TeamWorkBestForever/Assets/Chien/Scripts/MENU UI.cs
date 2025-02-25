5a0d3ed5b9c854b5a0d1a51ecefed687baefea25
DiverLight <tai874156@gmail.com>
2025-02-25 11:49:14 +0700

    UpChien
    ..

---
+++ TeamWorkBestForever/Assets/Chien/Scripts/MENU UI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MENUUI : MonoBehaviour
+{
+    public void PlayGame()  // Hàm phải là public
+    {
+        SceneManager.LoadScene("Map 1"); // Thay "Map 1" bằng đúng tên scene
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ TeamWorkBestForever/Assets/Chien/Scripts/MusicVolumeControl.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicVolumeControl : MonoBehaviour
+{
+    public Slider volumeSlider;  // Kéo thả Slider từ Inspector vào đây
+    private AudioSource musicSource;
+
+    void Start()
+    {
+        // Tìm AudioSource trong GameObject này
+        musicSource = GetComponent<AudioSource>();
+
+        // Gán giá trị mặc định từ PlayerPrefs nếu có
+        if (PlayerPrefs.HasKey("MusicVolume"))
+        {
+            float savedVolume = PlayerPrefs.GetFloat("MusicVolume");
+            musicSource.volume = savedVolume;
+            volumeSlider.value = savedVolume;
+        }
+        else
+        {
+            musicSource.volume = 1f;  // Giá trị mặc định là 100%
+            volumeSlider.value = 1f;
+        }
+
+        // Gán sự kiện khi thay đổi Slider
+        volumeSlider.onValueChanged.AddListener(ChangeVolume);
+    }
+
+    public void ChangeVolume(float volume)
+    {
+        musicSource.volume = volume;  // Cập nhật âm lượng
+        PlayerPrefs.SetFloat("MusicVolume", volume);  // Lưu lại giá trị âm lượng
+        PlayerPrefs.Save();
+    }
+}
